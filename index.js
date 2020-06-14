d608279e8dc543507e1d98bde00068b4f02ccd31
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-14 15:49:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => (document.body.innerHTML = json['id']))
+    .catch((err) => (document.body.innerHTML = err.message));
+}
